bb49053a421d9f1ecd13e224c456ec3824edc2a9
Nokweaya <nokweaya@gmail.com>
2023-07-21 21:00:15 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (k = 0 ; k <= 14 ; k++)
+		{
+			if (k > 9)
+				_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
