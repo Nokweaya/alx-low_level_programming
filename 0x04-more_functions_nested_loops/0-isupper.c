10263ee71806bf234192c0465c9d1dc336aa44de
Nokweaya <nokweaya@gmail.com>
2023-07-21 18:35:24 -0700

    0-supper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase character
+ * @c: parameter to be checked
+ * Return: 1 on success 0 on failure
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	       return (1);
+	else
+		return (0);
+}
