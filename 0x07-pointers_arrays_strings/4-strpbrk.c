58cdfb1c01530ab39ec1100bc094729c542e8212
Nokweaya <nokweaya@gmail.com>
2023-07-31 17:03:39 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
